6caa0c89aacc7bdbe204f4481a703426e2e8a07e
winters0727 <winters0727@gmail.com>
2022-02-04 00:18:39 +0900

    feat: 로그인 서비스, 컨트롤러, 라우터 초안 구현

---
+++ backend/controllers/login/index.js
@@ -0,0 +1,59 @@
+const jwt = require('jsonwebtoken');
+
+const userService = require('@/services/user');
+const hash = require('@/utils/hash');
+const token = require('@/utils/token');
+
+const loginMessage = require('./message');
+
+const TOKEN_MAXAGE = 86400 * 1000;
+
+exports.login = async (req, res) => {
+  try {
+    const payload = req.body;
+    const { userID, userPassword } = payload;
+    const hashedPassword = hash.createHashPassword(userPassword);
+
+    const data = await userService.getUserByUserID(userID);
+
+    if (data) {
+      const dataValues = data.dataValues;
+
+      if (hashedPassword === dataValues.userPassword) {
+        const jwtToken = await token.createToken({
+          id: dataValues.id,
+          userID: dataValues.userID,
+          nickname: dataValues.userNickname,
+        });
+
+        res.cookie('token', jwtToken, { maxAge: TOKEN_MAXAGE });
+        return res.status(200).json({ result: 'success' });
+      }
+      return res
+        .status(400)
+        .json({ result: 'fail', message: loginMessage.wrongPassword });
+    }
+
+    res
+      .status(400)
+      .json({ result: 'fail', message: loginMessage.userIDNotExist });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
+
+exports.isLogin = async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+    const token = req.cookies['token'];
+    if (token && jwt.verify(token, process.env.JWT_SECRET)) {
+      next();
+    } else {
+      return res
+        .status(401)
+        .json({ result: 'fail', message: loginMessage.unauthorized });
+    }
+  } catch (err) {
+    throw Error(err);
+  }
+};
---
+++ backend/controllers/login/message.js
@@ -0,0 +1,5 @@
+module.exports = {
+  userIDNotExist: '존재하지 않는 아이디입니다.',
+  wrongPassword: '비밀번호가 다릅니다',
+  unauthorized: '권한이 없습니다.',
+};
