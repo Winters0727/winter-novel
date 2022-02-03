506ba5c7278975eb0fa13aeb89a77b41d07ce90e
winters0727 <winters0727@gmail.com>
2022-02-03 19:50:18 +0900

    feat: 데이터베이스 설정 파일 생성

---
+++ backend/config/config.js
@@ -0,0 +1,18 @@
+require('../env');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'database_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
