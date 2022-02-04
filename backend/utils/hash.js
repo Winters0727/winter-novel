6a6e59261c4450c529f42fdc90c4b2b86eb10017
winters0727 <winters0727@gmail.com>
2022-02-04 15:34:07 +0900

    refactor: 비밀번호 암호화 방식을 단순 해시 함수에서 pbkdf2로 변경

--- backend/utils/hash.js
+++ backend/utils/hash.js
@@ -1,5 +1,8 @@
 const crypto = require('crypto');
 
 exports.createHashPassword = password => {
-  return crypto.createHash('sha512').update(password).digest('base64');
+  const salt = crypto.randomBytes(64);
+  return crypto
+    .pbkdf2Sync(password, salt.toString('base64'), 100000, 64, 'sha512')
+    .toString('base64');
 };
