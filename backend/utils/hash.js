27ca7e94c370ad1026f7da16ec1db932b978920c
winters0727 <winters0727@gmail.com>
2022-02-04 15:51:03 +0900

    fix: 비밀번호 암호화 함수에서 salt 변수를 전역 변수로 변경

--- backend/utils/hash.js
+++ backend/utils/hash.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto');
 
+const salt = crypto.randomBytes(64);
+
 exports.createHashPassword = password => {
-  const salt = crypto.randomBytes(64);
   return crypto
     .pbkdf2Sync(password, salt.toString('base64'), 100000, 64, 'sha512')
     .toString('base64');
