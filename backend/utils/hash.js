09af3120f4c5b0fe239ca2ff69cd23fa424a9134
winters0727 <winters0727@gmail.com>
2022-02-04 15:53:56 +0900

    refactor: 비밀번호 암호화 함수에서 salt를 랜덤바이트에서 환경 변수로 변경

--- backend/utils/hash.js
+++ backend/utils/hash.js
@@ -1,9 +1,7 @@
 const crypto = require('crypto');
 
-const salt = crypto.randomBytes(64);
-
 exports.createHashPassword = password => {
   return crypto
-    .pbkdf2Sync(password, salt.toString('base64'), 100000, 64, 'sha512')
+    .pbkdf2Sync(password, process.env.HASH_SALT, 100000, 64, 'sha512')
     .toString('base64');
 };
